6a96b6f6acb62b490b01842b4902e0b4af461b36
Martin Staykov <marti.staykov@gmail.com>
2020-04-27 15:47:25 +0300

    Made number of elements text bigger

--- src/components/UI/Slider/Slider.js
+++ src/components/UI/Slider/Slider.js
@@ -5,7 +5,7 @@ const Slider = (props) => {
 
     return (
       <div className="my-2">
-        <label >Number of elements</label>
+        <label className="h5">Number of elements</label>
         <input onChange={props.change} type="range" className="custom-range" min="5" max="100" step="1" id="customRange3"/>
       </div>
     );
